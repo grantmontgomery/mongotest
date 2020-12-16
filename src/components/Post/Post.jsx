42fea1979ab17d1d3daf0246c088cf0f469074d7
Grant <gmontgomery1994@gmail.com>
2020-12-15 21:25:10 -0800

    post section done.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import "./Post.css";
 
 export const Post = () => {
-  return <main></main>;
+  const [state, setState] = React.useState({ title: "", text: "" });
+
+  return (
+    <main className="postPageWrapper">
+      <section style={{ display: "flex", flexFlow: "column nowrap" }}>
+        <label htmlFor="">Title</label>
+        <input
+          type="text"
+          value={state.title}
+          onChange={({ target }) =>
+            setState((state) => ({ ...state, title: target.value }))
+          }
+        />
+        <label htmlFor="">Text</label>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          value={state.text}
+          onChange={({ target }) =>
+            setState((state) => ({ ...state, text: target.value }))
+          }
+        ></textarea>
+        <button style={{ marginTop: "50px", height: "50px", width: "100px" }}>
+          Submit
+        </button>
+      </section>
+    </main>
+  );
 };
